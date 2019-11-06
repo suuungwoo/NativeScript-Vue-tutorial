e769c8bcb60ca50106421308c9b7a7b13f0eeb3a
suuungwoo <suuungwoo@gmail.com>
2019-11-06 13:36:14 +0900

    基本的なデザイン

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,32 +1,36 @@
 <template>
-    <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!"/>
-        <GridLayout columns="*" rows="*">
-            <Label class="message" :text="msg" col="0" row="0"/>
-        </GridLayout>
-    </Page>
+  <Page>
+    <ActionBar title="My Tasks" />
+    <TabView androidTabsPosition="bottom">
+      <TabViewItem title="To Do">
+        <StackLayout>
+          <Label text="This tab will list active tasks and will let users add new tasks." textWrap="true" />
+        </StackLayout>
+      </TabViewItem>
+      <TabViewItem title="Completed">
+        <StackLayout>
+          <Label text="This tab will list completed tasks for tracking." textWrap="true" />
+        </StackLayout>
+      </TabViewItem>
+    </TabView>
+
+  </Page>
 </template>
 
 <script >
-  export default {
-    data() {
-      return {
-        msg: 'Hello World!'
-      }
-    }
+export default {
+  data() {
+    return {
+      msg: "Hello World!"
+    };
   }
+};
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
-    }
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
 
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
 </style>
